dc829b0716a48c6d345cf3f574b538ec4aaba919
sepalaznik <se.palaznik@yandex.by>
2021-11-08 20:32:01 +0200

    dinymic render restaurants and menu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,16 +1,61 @@
-const restourant = "gusi-lebedi";
+const cardsMenu = document.querySelector(".cards-menu");
+
+const changeTitle = (restaurant) => {
+    const restaurantTitle = document.querySelector(".restaurant-title");
+    restaurantTitle.textContent = restaurant.name;
+
+    const restaurantRating = document.querySelector(".rating");
+    restaurantRating.textContent = restaurant.stars;
+
+    const restaurantPrice = document.querySelector(".price");
+    restaurantPrice.textContent = `От ${restaurant.price} ₽`;
+
+    const restaurantKitchen = document.querySelector(".category");
+    restaurantKitchen.textContent = restaurant.kitchen;
+}
 
 const renderItem = (data) => {
-    data.forEach(element => {
-        console.log(element)
+    data.forEach(({ name, image, description, price, id }) => {
+        const card = document.createElement("div");
+
+        card.classList.add("card");
+
+        card.innerHTML = `
+            <img src="${image}" alt="${name}" class="card-image" />
+            <div class="card-text">
+                <div class="card-heading">
+                    <h3 class="card-title card-title-reg">${name}</h3>
+                </div>
+                <div class="card-info">
+                    <div class="ingredients">${description}</div>
+                </div>
+                <div class="card-buttons">
+                    <button class="button button-primary button-add-cart">
+                        <span class="button-card-text">В корзину</span>
+                        <span class="button-cart-svg"></span>
+                    </button>
+                    <strong class="card-price-bold">${price} ₽</strong>
+                </div>
+            </div>
+        `;
+
+        cardsMenu.append(card);
     });
 };
 
-fetch(`https://js-service-delivery-default-rtdb.firebaseio.com/db/${restourant}.json`)
+if (localStorage.getItem("restaurant")) {
+    const restaurant = JSON.parse(localStorage.getItem("restaurant"));
+
+    changeTitle(restaurant);
+
+    fetch(`https://js-service-delivery-default-rtdb.firebaseio.com/db/${restaurant.products}`)
     .then((response) => response.json())
     .then((data) => {
         renderItem(data)
     })
     .catch((error) => {
-        console.error(error)
-    });
+        console.error(error);
+    })
+} else {
+    window.location.href = "/"
+};
